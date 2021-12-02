e136335817c08518a1603487d9507d0b1134618d
Ionut Ciovica <ionut.ciovica@gmail.com>
2021-12-02 01:46:38 +0000

    Measuring performance.

---
+++ UltimatePortfolioTest/PerformanceTests.swift
@@ -0,0 +1,28 @@
+//
+//  PerformanceTests.swift
+//  UltimatePortfolioTest
+//
+//  Created by Ionut Ciovica on 02/12/2021.
+//
+
+import XCTest
+@testable import Ultimate_Portfolio
+
+class PerformanceTests: BaseTestCase {
+    func testAwardCalculationPerformance() throws {
+        // Create a significant amount of test data
+        for _ in 1...100 {
+            try dataController.createSampleData()
+        }
+        // Simulate lots of awards to check
+        let awards = Array(repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(
+            awards.count,
+            500,
+            "This checks the number of awards is constant. Change this if you add new awards."
+        )
+        measure {
+            _ = awards.filter(dataController.hasEarned)
+        }
+    }
+}
